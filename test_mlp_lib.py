bff146c256c0ef4ea4007ff6a6d02c603527892a
Quang <quangnb2000@gmail.com>
2020-09-10 16:07:18 +0700

    MLP to Pi

--- test_mlp_lib.py
+++ test_mlp_lib.py
@@ -1,14 +1,14 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
-testdata = pd.read_excel('testdata.xlsx', header= None, usecols='A:V')
-test_labels = pd.read_excel('testdata.xlsx', header = None, usecols= 'W')
+testdata = pd.read_excel('testset.xlsx', header= None, usecols='A:V')
+test_labels = pd.read_excel('testset.xlsx', header = None, usecols= 'W')
 # print(mlp.coefs_)
 # print(type(mlp.coef))
 
 W = []
 b = []
-for i in range(5):
+for i in range(2):
     W.append(np.loadtxt('mlpcoefs{0}{1}.csv'.format(i + 1,i + 2),delimiter=','))
     k = np.loadtxt('mlpbias{0}{1}.csv'.format(i + 1, i + 2), delimiter=',')
     # print(k.shape)
@@ -16,7 +16,7 @@ for i in range(5):
     b.append(k)
     # b[-1] = k
 A = [testdata.to_numpy()]
-for i in range(5):
+for i in range(2):
     A.append(A[i].dot(W[i]) + b[i].reshape((b[i].shape[0],)))
     A[-1] = np.maximum(0, A[-1])
 test_labels = test_labels.to_numpy()
